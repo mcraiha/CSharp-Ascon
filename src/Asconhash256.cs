8b2e26120de51beef72eb21662522edae1424148
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2026-05-25 15:22:02 +0300

    WIP of Asconhash256
    More to come

---
+++ src/Asconhash256.cs
@@ -0,0 +1,233 @@
+using System.Numerics;
+using System.Buffers.Binary;
+
+namespace CSAscon;
+
+/// <summary>
+/// Asconhash256, version 1.3.0
+/// </summary>
+public static class Asconhash256
+{
+	/// <summary>
+	/// Version number of Ascon
+	/// </summary>
+	public const string CRYPTO_VERSION = "1.3.0";
+	
+	public const int CRYPTO_BYTES = 32; // 256 bits
+	public const int ASCON_HASH_BYTES = 32; /* HASH */
+	public const int ASCON_HASH_ROUNDS = 12;
+	public const int ASCON_VARIANT = 2;
+
+	public const int ASCON_HASH_RATE = 8;
+
+	private const byte RC0 = 0xf0;
+	private const byte RC1 = 0xe1;
+	private const byte RC2 = 0xd2;
+	private const byte RC3 = 0xc3;
+	private const byte RC4 = 0xb4;
+	private const byte RC5 = 0xa5;
+	private const byte RC6 = 0x96;
+	private const byte RC7 = 0x87;
+	private const byte RC8 = 0x78;
+	private const byte RC9 = 0x69;
+	private const byte RCa = 0x5a;
+	private const byte RCb = 0x4b;
+
+	/// <summary>
+	/// Ascon state, known as S
+	/// </summary>
+	private struct ascon_state_t
+	{
+		public ulong[] x = new ulong[5];
+
+		public ascon_state_t()
+		{
+
+		}
+	}
+
+	private static void ascon_inithash(ref ascon_state_t s)
+	{
+		/* initialize */
+		s.x[0] = 0x9b1e5494e934d681;
+		s.x[1] = 0x4bc3a01e333751d2;
+		s.x[2] = 0xae65396c6b34b81a;
+		s.x[3] = 0x3c7fd4a4d56a4db3;
+		s.x[4] = 0x1a5c464906c5976d;
+	}
+
+	private static void ascon_absorb(ref ascon_state_t s, ReadOnlyMemory<byte> input, ulong inlen)
+	{
+		/* absorb full plaintext blocks */
+		int offset = 0;
+		while (inlen >= ASCON_HASH_RATE)
+		{
+			s.x[0] ^= LOAD(input.Slice(offset), 8);
+			P(s, ASCON_HASH_ROUNDS);
+			offset += ASCON_HASH_RATE;
+			inlen -= ASCON_HASH_RATE;
+		}
+
+		/* absorb final plaintext block */
+		s.x[0] ^= LOAD(input.Slice(offset), (int)inlen);
+		s.x[0] ^= PAD((int)inlen);
+	}
+
+	private static void ascon_squeeze(ref ascon_state_t s, Memory<byte> output, ulong outlen)
+	{
+		/* squeeze full output blocks */
+		P(s, 12);
+		int offset = 0;
+		while (outlen > ASCON_HASH_RATE)
+		{
+			STOREBYTES(output.Slice(offset), s.x[0], 8);
+			P(s, ASCON_HASH_ROUNDS);
+			offset += ASCON_HASH_RATE;
+			outlen -= ASCON_HASH_RATE;
+		}
+		/* squeeze final output block */
+		STOREBYTES(output.Slice(offset), s.x[0], (int)outlen);
+	}
+
+	private static int ascon_xof(Memory<byte> output, ulong outlen, ReadOnlyMemory<byte> input, ulong inlen) 
+	{
+		ascon_state_t s = new ascon_state_t();
+
+		ascon_inithash(ref s);
+		ascon_absorb(ref s, input, inlen);
+		ascon_squeeze(ref s, output, outlen);
+
+		return 0;
+	}
+
+	public static int crypto_hash(Memory<byte> output, ReadOnlyMemory<byte> input, ulong inlen)
+	{
+		return ascon_xof(output, CRYPTO_BYTES, input, inlen);
+	}
+
+	private static void P(ascon_state_t s, int nr) 
+	{
+		if (nr == 12) P12ROUNDS(s);
+		if (nr == 8) P8ROUNDS(s);
+		if (nr == 6) P6ROUNDS(s);
+	}
+
+	private static void P12ROUNDS(ascon_state_t s) 
+	{
+		ROUND(s, RC0);
+		ROUND(s, RC1);
+		ROUND(s, RC2);
+		ROUND(s, RC3);
+		ROUND(s, RC4);
+		ROUND(s, RC5);
+		ROUND(s, RC6);
+		ROUND(s, RC7);
+		ROUND(s, RC8);
+		ROUND(s, RC9);
+		ROUND(s, RCa);
+		ROUND(s, RCb);
+	}
+
+	private static void P8ROUNDS(ascon_state_t s) 
+	{
+		ROUND(s, RC4);
+		ROUND(s, RC5);
+		ROUND(s, RC6);
+		ROUND(s, RC7);
+		ROUND(s, RC8);
+		ROUND(s, RC9);
+		ROUND(s, RCa);
+		ROUND(s, RCb);
+	}
+
+	private static void P6ROUNDS(ascon_state_t s) 
+	{
+		ROUND(s, RC6);
+		ROUND(s, RC7);
+		ROUND(s, RC8);
+		ROUND(s, RC9);
+		ROUND(s, RCa);
+		ROUND(s, RCb);
+	}
+
+	private static void ROUND(ascon_state_t s, byte C) 
+	{
+		ascon_state_t t = new ascon_state_t();
+		/* round constant */
+		s.x[2] ^= C;
+		/* s-box layer */
+		s.x[0] ^= s.x[4];
+		s.x[4] ^= s.x[3];
+		s.x[2] ^= s.x[1];
+		t.x[0] = s.x[0] ^ (~s.x[1] & s.x[2]);
+		t.x[2] = s.x[2] ^ (~s.x[3] & s.x[4]);
+		t.x[4] = s.x[4] ^ (~s.x[0] & s.x[1]);
+		t.x[1] = s.x[1] ^ (~s.x[2] & s.x[3]);
+		t.x[3] = s.x[3] ^ (~s.x[4] & s.x[0]);
+		t.x[1] ^= t.x[0];
+		t.x[3] ^= t.x[2];
+		t.x[0] ^= t.x[4];
+		/* linear layer */
+		s.x[2] = t.x[2] ^ ROR(t.x[2], 6 - 1);
+		s.x[3] = t.x[3] ^ ROR(t.x[3], 17 - 10);
+		s.x[4] = t.x[4] ^ ROR(t.x[4], 41 - 7);
+		s.x[0] = t.x[0] ^ ROR(t.x[0], 28 - 19);
+		s.x[1] = t.x[1] ^ ROR(t.x[1], 61 - 39);
+		s.x[2] = t.x[2] ^ ROR(s.x[2], 1);
+		s.x[3] = t.x[3] ^ ROR(s.x[3], 10);
+		s.x[4] = t.x[4] ^ ROR(s.x[4], 7);
+		s.x[0] = t.x[0] ^ ROR(s.x[0], 19);
+		s.x[1] = t.x[1] ^ ROR(s.x[1], 39);
+		s.x[2] = ~s.x[2];
+	}
+
+	private static ulong PAD(int i) 
+	{ 
+		return 0x01ul << (8 * i);
+	}
+
+	private static ulong MASK(int n) 
+	{
+		/* undefined for n == 0 */
+		return ~0ul << (64 - 8 * n);
+	}
+
+	private static ulong LOAD(ReadOnlyMemory<byte> bytes, int n) 
+	{
+		if (n < 8)
+		{
+			Span<byte> tempArray = stackalloc byte[8];
+			bytes.Span.CopyTo(tempArray);
+			
+			return BitConverter.ToUInt64(tempArray);
+		}
+		else 
+		{
+			return BitConverter.ToUInt64(bytes.Span);
+		}
+	}
+
+	private static ulong LOADBYTES(byte[] bytes, int n) 
+	{
+		return BitConverter.ToUInt64(bytes);
+	}
+
+	private static void STOREBYTES(Memory<byte> bytes, ulong w, int n) 
+	{
+		Span<byte> tempArray = stackalloc byte[8];
+		BinaryPrimitives.WriteUInt64LittleEndian(tempArray, w);
+		if (n == 8)
+		{
+			tempArray.CopyTo(bytes.Span);
+		}
+		else
+		{
+			tempArray.Slice(0, n).CopyTo(bytes.Span);
+		}
+	}
+
+	private static ulong ROR(ulong x, int n) 
+	{
+		return BitOperations.RotateRight(x, n);
+	}
+}
